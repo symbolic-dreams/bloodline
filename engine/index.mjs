06f478ee2c050c44a9a26554bffef82c652aefda
Michael Haufe <tno@thenewobjective.com>
2025-12-27 18:03:13 -0600

    Lesson 1 (#21)
    * Lesson 1
    
    * Remove redundant script tag for module import in index.html

--- engine/index.mjs
+++ engine/index.mjs
@@ -1,1 +1,16 @@
-export class Engine { }
+import { TileInteractor } from "./application/TileInteractor.mjs";
+import { TileRepository } from "./data/TileRepository.mjs";
+import { TilePresenter } from "./presentation/TilePresenter.mjs";
+
+export class Engine {
+    constructor({ baseUrl, size, elContainer }) {
+        this.interactor = new TileInteractor({
+            repository: new TileRepository({ baseUrl, size }),
+            presenter: new TilePresenter(elContainer)
+        });
+    }
+
+    render(...tileIds) {
+        tileIds.forEach(id => this.interactor.renderTile(id));
+    }
+}
